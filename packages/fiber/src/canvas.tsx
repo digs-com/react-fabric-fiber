3a7be0b68c86dbaf95115fb9f0be30ad3a161dc6
Nick DeGroot <1966472+nickthegroot@users.noreply.github.com>
2024-02-15 11:31:50 -0800

    :sparkles: Add onCreated canvas prop

--- packages/fiber/src/canvas.tsx
+++ packages/fiber/src/canvas.tsx
@@ -14,15 +14,20 @@ import { ReconcilerRoot, createRoot } from "./root";
 
 interface Props extends Partial<fabric.ICanvasOptions>, PropsWithChildren {
   resize?: ResizeOptions;
+
+  /**
+   * Callback to be called when the canvas is created or changed.
+   */
+  onCreated?: (canvas: fabric.Canvas) => void;
 }
 
 export const Canvas = forwardRef<HTMLCanvasElement, Props>(function Canvas(
-  { children, resize, ...canvasProps },
+  { children, resize, onCreated, ...canvasProps },
   forwardedRef
 ) {
   const canvasRef = useRef<HTMLCanvasElement>(null!);
-
   useImperativeHandle(forwardedRef, () => canvasRef.current);
+
   const [containerRef, containerRect] = useMeasure({
     scroll: true,
     debounce: { scroll: 50, resize: 0 },
@@ -30,12 +35,15 @@ export const Canvas = forwardRef<HTMLCanvasElement, Props>(function Canvas(
   });
 
   const root = useRef<ReconcilerRoot | null>(null);
-
   useLayoutEffect(() => {
     const canvas = canvasRef.current;
     if (!canvas || !containerRect.width || !containerRect.height) return;
 
-    root.current ??= createRoot(canvas, canvasProps);
+    if (!root.current) {
+      root.current = createRoot(canvas, canvasProps);
+      onCreated?.(root.current.canvas);
+    }
+
     root.current.resize(containerRect);
     root.current.render(
       <CanvasProvider root={root.current}>{children}</CanvasProvider>
